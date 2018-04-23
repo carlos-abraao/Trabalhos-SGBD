70e4b7ce7133d2afddeef5becd4e00d236b26643
Leonardo Monteiro <leonardomm037@gmail.com>
2018-04-23 18:40:46 -0300

    Inicio do segundo trabalho

---
+++ Trabalho 2/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	
+	//Structs
+	struct registro{
+		int id;
+		string nome;
+		string sobrenome;
+		string cpf;
+	}
+	struct pagina{
+		registro registros[1000];
+	}
+	struct tabela{
+		pagina *paginas;
+		int qtd_paginas;
+	}
+	
+	//Pesquisar mais, estrutura de índice:    std::map<int chave, int id_pag, int id_reg,> indice;
+
+
+	//Abrindo o arquivo	
+	ifstream arquivo;
+	string linha;
+
+	arquivo.open("nome.txt");
+
+	//Importando os registros
+	if(arquivo.is_open()){
+		while(getline(arquivo,linha)){
+			//Código de importação, aguardando pela organização
+		}
+	}else{
+		cout << "ERRO: Não foi possivel abrir o arquivo!" << endl;
+	}
+
+	//Continuação do código
+}
