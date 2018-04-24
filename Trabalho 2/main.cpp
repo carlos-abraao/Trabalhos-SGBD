ad21509f3e2d59b62ceebefc65496fe6352feee8
carlos-abraao <carlos.abraao.sa@gmail.com>
2018-04-24 10:34:48 -0300

    mudanças na main

--- Trabalho 2/main.cpp
+++ Trabalho 2/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char const *argv[]){
-	
-	//Structs
-	struct registro{
-		int id;
-		string nome;
-		string sobrenome;
-		string cpf;
-	}
-	struct pagina{
-		registro registros[1000];
-	}
-	struct tabela{
-		pagina *paginas;
-		int qtd_paginas;
-	}
-	
-	//Pesquisar mais, estrutura de índice:    std::map<int chave, int id_pag, int id_reg,> indice;
+//Structs
 
+struct registro{
+	int id;
+	string nome;
+	string sobrenome;
+	string cpf;
+};
 
+struct pagina{
+	registro registros[1000];
+};
+
+struct tabela{
+	pagina *paginas;
+	int qtd_paginas;
+};
+
+//Pesquisar mais, estrutura de índice:    std::map<int chave, int id_pag, int id_reg,> indice;
+
+int main(int argc, char const *argv[]){	
 	//Abrindo o arquivo	
 	ifstream arquivo;
 	string linha;
